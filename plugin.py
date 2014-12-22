858159a17206c346a3b7619273feb6bc4dab686c
Dozodouzo <park425@outlook.com>
2014-12-22 03:11:29 -0800

    plsgod

--- plugin.py
+++ plugin.py
@@ -39,7 +39,7 @@ the_message = 'more like bozo amirite'
 class dozo(callbacks.Plugin):
     """Add the help for "@plugin help Dozo-plugin" here
     This should describe *how* to use this plugin."""
-    def __init__(self, irc, msg):
+    def __init__(self, irc):
         callbacks.Plugin.__init__(self, irc, msg)
 		
 	def dozo(self, irc, msg):
