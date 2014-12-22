1b6fc724443d4d29c1d67e34c5ccb28dc77bd52f
Dozodouzo <park425@outlook.com>
2014-12-22 02:19:48 -0800

    i wish i knew python lol

--- plugin.py
+++ plugin.py
@@ -39,8 +39,8 @@ the_message = 'more like bozo amirite'
 class dozo(callbacks.Plugin):
     """Add the help for "@plugin help Dozo-plugin" here
     This should describe *how* to use this plugin."""
-    def __init__(irc):
-        callbacks.Plugin.__init__(irc)
+    def __init__(irc, msg):
+        callbacks.Plugin.__init__(irc, msg)
 		
 	def dozo(irc, msg):
 		irc.reply{the_message}
