7aed95815e4040b1c59e9106aec031a2066228ff
Dozodouzo <park425@outlook.com>
2014-12-22 02:55:10 -0800

    this is getting stupid

--- plugin.py
+++ plugin.py
@@ -39,10 +39,10 @@ the_message = 'more like bozo amirite'
 class dozo(callbacks.Plugin):
     """Add the help for "@plugin help Dozo-plugin" here
     This should describe *how* to use this plugin."""
-    def __init__(irc, msg):
-        callbacks.Plugin.__init__(irc, msg)
+    def __init__(self, irc):
+        callbacks.Plugin.__init__(self, irc)
 		
-	def dozo(irc, msg):
+	def dozo(self, irc, msg):
 		irc.reply{"more like bozo amirite"}
 
 
