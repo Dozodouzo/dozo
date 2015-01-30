056e0c1b3b9b7e486b607cf8227e4fb8efe1564c
Dozodouzo <park425@outlook.com>
2015-01-29 22:45:03 -0800

    ayylmao

--- plugin.py
+++ plugin.py
@@ -154,6 +154,30 @@ class dozo(callbacks.Plugin):
         irc.reply('Eien stinx', prefixNick=False)
     eien = wrap(eien)
 
+    def ayylmao(self, irc, msg, args):
+        """
+        kek
+        """
+        irc.reply('░░░░░░▄▀▒▒▒▒░░░░▒▒▒▒▒▒▒▒▒▒▒▒▒█', prefixNick=False)
+        irc.reply('░░░░░█▒▒▒▒░░░░▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█', prefixNick=False)
+        irc.reply('░░░░█▒▒▄▀▀▀▀▀▄▄▒▒▒▒▒▒▒▒▒▄▄▀▀▀▀▀▀▄', prefixNick=False)
+        irc.reply('░░▄▀▒▒▒▄█████▄▒█▒▒▒▒▒▒▒█▒▄█████▄▒█', prefixNick=False)
+        irc.reply('░█▒▒▒▒▐██▄████▌▒█▒▒▒▒▒█▒▐██▄████▌▒█', prefixNick=False)
+        irc.reply('▀▒▒▒▒▒▒▀█████▀▒▒█▒░▄▒▄█▒▒▀█████▀▒▒▒█', prefixNick=False)
+        irc.reply('▒▒▐▒▒▒░░░░▒▒▒▒▒█▒░▒▒▀▒▒█▒▒▒▒▒▒▒▒▒▒▒▒█', prefixNick=False)
+        irc.reply('▒▌▒▒▒░░░▒▒▒▒▒▄▀▒░▒▄█▄█▄▒▀▄▒▒▒▒▒▒▒▒▒▒▒▌', prefixNick=False)
+        irc.reply('▒▌▒▒▒▒░▒▒▒▒▒▒▀▄▒▒█▌▌▌▌▌█▄▀▒▒▒▒▒▒▒▒▒▒▒▐', prefixNick=False)
+        irc.reply('▒▐▒▒▒▒▒▒▒▒▒▒▒▒▒▌▒▒▀███▀▒▌▒▒▒▒▒▒▒▒▒▒▒▒▌', prefixNick=False)
+        irc.reply('▀▀▄▒▒▒▒▒▒▒▒▒▒▒▌▒▒▒▒▒▒▒▒▒▐▒▒▒▒▒▒▒▒▒▒▒█', prefixNick=False)
+        irc.reply('▀▄▒▀▄▒▒▒▒▒▒▒▒▐▒▒▒▒▒▒▒▒▒▄▄▄▄▒▒▒▒▒▒▄▄▀', prefixNick=False)
+        irc.reply('▒▒▀▄▒▀▄▀▀▀▄▀▀▀▀▄▄▄▄▄▄▄▀░░░░▀▀▀▀▀▀', prefixNick=False)
+        irc.reply('▒▒▒▒▀▄▐▒▒▒▒▒▒▒▒▒▒▒▒▒▐', prefixNick=False)
+        irc.reply('░▄▄▄░░▄░░▄░▄░░▄░░▄░░░░▄▄░▄▄░░░▄▄▄░░░▄▄▄', prefixNick=False)
+        irc.reply('█▄▄▄█░█▄▄█░█▄▄█░░█░░░█░░█░░█░█▄▄▄█░█░░░█', prefixNick=False)
+        irc.reply('█░░░█░░█░░░░█░░░░█░░░█░░█░░█░█░░░█░█░░░█', prefixNick=False)
+        irc.reply('▀░░░▀░░▀░░░░▀░░░░▀▀▀░░░░░░░░░▀░░░▀░▀▄▄▄▀', prefixNick=False)
+    ayylmao = wrap(ayylmao)
+
 Class = dozo
 
 
